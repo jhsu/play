22e1b576d61c39cf9ad99073076ac3298608a5c8
Joseph Hsu <jhsu.x1@gmail.com>
2012-09-10 11:14:37 -0400

    figure out song name from file if needed

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,10 +5,17 @@ class Song
   # @param [MPD::Song] song
   def initialize(mpd_song)
     @artist = mpd_song.artist
-    @name = mpd_song.title
+    @name = mpd_song.title || song_name_from_file(mpd_song.file)
     @album = mpd_song.album
 
     @id = mpd_song.id
     @pos = mpd_song.pos
+  end
+
+  private
+
+  def song_name_from_file(file)
+    file.split("/").last[/(.*)\.\w+/]
+    $1
   end
 end
