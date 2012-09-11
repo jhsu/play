c65c68b97981e45f5be13dfd88474320178b2315
Joseph Hsu <jhsu.x1@gmail.com>
2012-09-11 16:59:33 -0400

    add more info routes and update autodj

--- app/api/system.rb
+++ app/api/system.rb
@@ -1,8 +1,12 @@
 module Play
   class App < Sinatra::Base
     get "/streaming_info" do
-      streaming_data = {:stream_url => Play.config.stream_url, :pusher_key => Play.config.pusher_key}
+      streaming_data = {:stream_url => Play.config.stream_url}
       streaming_data.to_json
+    end
+
+    get "/stream_url" do
+      {:stream_url => Play.config.stream_url}.to_json
     end
   end
 end
